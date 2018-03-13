80cfdb0f08f19c4de32141af6de129a73d378e0f
VladimirBerdnik <v.berdnik@saritasa.com>
2018-03-13 22:04:04 +0600

    Improve user with command communication

--- src/Commands/FormRequestsScaffoldCommand.php
+++ src/Commands/FormRequestsScaffoldCommand.php
@@ -4,6 +4,7 @@ namespace Saritasa\LaravelTools\Commands;
 
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 use Saritasa\LaravelTools\Services\FormRequestService;
 
 /**
@@ -53,10 +54,17 @@ class FormRequestsScaffoldCommand extends Command
      */
     public function handle()
     {
-        $formRequestClassName = $this->getFormRequestClassName();
         $modelClassName = $this->getModelClass();
+        $formRequestClassName = $this->getFormRequestClassName();
 
-        $this->formRequestService->generateFormRequest($modelClassName, $formRequestClassName);
+        if (!$formRequestClassName) {
+            $suggestedClassName = $this->generateFormRequestClassName($modelClassName);
+            $formRequestClassName = $this->ask('Please, enter new form request class name', $suggestedClassName);
+        }
+
+        $resultFileName = $this->formRequestService->generateFormRequest($modelClassName, $formRequestClassName);
+
+        $this->info("Check out generated file [{$resultFileName}]");
     }
 
     /**
@@ -67,6 +75,20 @@ class FormRequestsScaffoldCommand extends Command
     protected function getFormRequestClassName(): ?string
     {
         return $this->argument('request');
+    }
+
+    /**
+     * Generate form request class name from model class name.
+     *
+     * @param string $modelClassName Model class name to generate form request for
+     *
+     * @return string
+     */
+    protected function generateFormRequestClassName(string $modelClassName)
+    {
+        $formRequestClassName = $modelClassName . 'Request';
+
+        return Str::studly($formRequestClassName);
     }
 
     /**
