ef22bc2917933a62697fd0c25e5215ba3a21d39d
VladimirBerdnik <v.berdnik@saritasa.com>
2018-03-26 16:43:17 +0600

    Improve getters and setters generators with function separating

--- src/CodeGenerators/GetterGenerator.php
+++ src/CodeGenerators/GetterGenerator.php
@@ -3,34 +3,62 @@
 namespace Saritasa\LaravelTools\CodeGenerators;
 
 /**
- * Getter Generator class. Allows to generate getter function code for given attribute and type.
+ * Getter Generator class. Allows to generate getter function declaration for given attribute and type.
  */
 class GetterGenerator
 {
     /**
-     * Allows to generate getter function code for given attribute and type.
+     * Allows to generate getter function declaration for given attribute and type.
      *
      * @param string $attributeName Attribute name for which need to generate getter
-     * @param string $valueType Attribute type to typehint getter
+     * @param string $attributeType Attribute type to typehint getter
      *
      * @return string
      */
-    public function render(string $attributeName, string $valueType): string
+    public function render(string $attributeName, string $attributeType): string
+    {
+        return implode("\n", [
+            $this->getDescription($attributeName, $attributeType),
+            $this->getDeclaration($attributeName, $attributeType),
+        ]);
+    }
+
+    /**
+     * Returns getter function declaration.
+     *
+     * @param string $attributeName Attribute name for which need to generate getter
+     * @param string $attributeType Attribute type to typehint getter
+     *
+     * @return string
+     */
+    protected function getDeclaration(string $attributeName, string $attributeType): string
     {
         $getterFunctionName = 'get' . ucfirst($attributeName);
 
-        $getterFunctionCode = <<<template
-/**
- * Get {$attributeName} attribute value.
- *
- * @returns {$valueType}
- */
- public function {$getterFunctionName}(): {$valueType}
+        return <<<template
+ public function {$getterFunctionName}(): {$attributeType}
  {
      return \$this->{$attributeName};
  }
 template;
+    }
 
-        return $getterFunctionCode;
+    /**
+     * Returns getter function description.
+     *
+     * @param string $attributeName Attribute name for which need to generate getter
+     * @param string $attributeType Attribute type to typehint getter
+     *
+     * @return string
+     */
+    protected function getDescription(string $attributeName, string $attributeType): string
+    {
+        return <<<template
+/**
+ * Get {$attributeName} attribute value.
+ *
+ * @returns {$attributeType}
+ */
+template;
     }
 }
--- src/CodeGenerators/SetterGenerator.php
+++ src/CodeGenerators/SetterGenerator.php
@@ -3,34 +3,64 @@
 namespace Saritasa\LaravelTools\CodeGenerators;
 
 /**
- * Setter Generator class. Allows to generate setter function code for given attribute and type.
+ * Setter Generator class. Allows to generate setter function declaration for given attribute and type.
  */
 class SetterGenerator
 {
     /**
-     * Allows to generate setter function code for given attribute and type.
+     * Allows to generate setter function declaration for given attribute and type.
      *
      * @param string $attributeName Attribute name for which need to generate setter
-     * @param string $valueType Attribute type to typehint value
+     * @param string $attributeType Attribute type to typehint value
      *
      * @return string
      */
-    public function render(string $attributeName, string $valueType): string
+    public function render(string $attributeName, string $attributeType): string
+    {
+        return implode("\n", [
+            $this->getDescription($attributeName, $attributeType),
+            $this->getDeclaration($attributeName, $attributeType),
+        ]);
+    }
+
+    /**
+     * Returns setter function declaration.
+     *
+     * @param string $attributeName Attribute name for which need to generate setter
+     * @param string $attributeType Attribute type to typehint value
+     *
+     * @return string
+     */
+    protected function getDeclaration(string $attributeName, string $attributeType): string
     {
         $setterFunctionName = 'set' . ucfirst($attributeName);
 
-        $setterFunctionCode = <<<template
-/**
- * Set {$attributeName} attribute value.
- *
- * @returns void
- */
- public function {$setterFunctionName}({$valueType} \${$attributeName}): void
+        return <<<template
+ public function {$setterFunctionName}({$attributeType} \${$attributeName}): void
  {
      \$this->{$attributeName} = \${$attributeName};
  }
 template;
+    }
 
-        return $setterFunctionCode;
+    /**
+     * Returns setter function description.
+     *
+     * @param string $attributeName Attribute name for which need to generate setter
+     * @param string $attributeType Attribute type to typehint value
+     *
+     * @return string
+     */
+    protected function getDescription(string $attributeName, string $attributeType): string
+    {
+        return <<<template
+/**
+ * Set {$attributeName} attribute value.
+ *
+ * @param {$attributeType} \${$attributeName} New attribute value
+ *
+ * @returns void
+ */
+template;
     }
 }
