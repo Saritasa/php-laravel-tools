c2c927515bba238cb6dee9b23223822696dd8bbe
VladimirBerdnik <v.berdnik@saritasa.com>
2018-04-25 16:37:41 +0600

    Improve class phpdoc property parenthesis

--- src/DTO/PhpClasses/ClassPhpDocPropertyObject.php
+++ src/DTO/PhpClasses/ClassPhpDocPropertyObject.php
@@ -7,7 +7,7 @@ use Saritasa\LaravelTools\Enums\PhpDocPropertyAccessTypes;
 /**
  * PHPDoc class property details. Stores information about class property, declared in PhpDoc class' section.
  */
-class ClassPhpDocPropertyObject extends ClassPropertyObject
+class ClassPhpDocPropertyObject extends VariableObject
 {
     const ACCESS_TYPE = 'accessType';
 
